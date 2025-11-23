c36bc3c37ff040e574921aff44286246fc9f7fdd
Charls Cabangon <charlsmercabangon@gmail.com>
2025-11-23 21:52:29 +0800

    chore: create data dir and separate data files

---
+++ src/data/experiences.js
@@ -0,0 +1,38 @@
+export const experiences = [
+  {
+    id: 0,
+    title: 'Frontend Engineer',
+    company: 'Apple',
+    date: '2028 -  ∞',
+    isActive: true,
+    dir: 'exp',
+    filename: 'frontend.md',
+  },
+  {
+    id: 1,
+    title: 'Graphic Artist',
+    company: 'Freelance',
+    date: '2024 - ∞',
+    isActive: true,
+    dir: 'exp',
+    filename: 'graphics.md',
+  },
+  {
+    id: 2,
+    title: 'Intern',
+    company: 'Aklan Provincial ICT Office',
+    date: 'January 2025 - March 2025',
+    isActive: false,
+    dir: 'exp',
+    filename: 'intern.md',
+  },
+  {
+    id: 3,
+    title: 'BS in Computer Science',
+    company: 'Garcia College of Technology',
+    date: 'August 2021 - May 2025',
+    isActive: false,
+    dir: 'exp',
+    filename: 'grad.md',
+  },
+];
---
+++ src/data/projects.js
@@ -0,0 +1,9 @@
+export const projects = [
+  {
+    id: 1,
+    title: 'Reaux',
+    description: 'A mock premium jewelry botique',
+    image: 'https://images.unsplash.com/photo-1618761714954-0b8cd0026356?w=672&h=494&fit=crop',
+    link: '',
+  },
+];
